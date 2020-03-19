dc3699035980f301cfdbc42da51e127533aa5092
Sukanya068 <ausukanya@hotmail.com>
2020-03-19 21:16:04 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyDqu8_8GDj2tctIMZVhQQbFYcTLHBc0Uuw",
+    authDomain: "todo-81e16.firebaseapp.com",
+    databaseURL: "https://todo-81e16.firebaseio.com",
+    projectId: "todo-81e16",
+    storageBucket: "todo-81e16.appspot.com",
+    messagingSenderId: "816112037601",
+    appId: "1:816112037601:web:e747dbb3af2283faaad1a0",
+    measurementId: "G-G0W2KKP7YP"
+  };
+
+  export default config;
